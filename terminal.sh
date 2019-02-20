78f3b4724c99907a8e872795aab0570c5c5614f9
neeshsilva <neeshsilva1@gmail.com>
2019-02-20 10:58:46 +0530

    version 0.02

---
+++ terminal.sh
@@ -0,0 +1,25 @@
+version="0.02"
+
+git add .
+
+git commit -m "version $version"
+
+git push origin development
+
+git checkout -b release/$version
+
+git add .
+
+git commit -m "new version is added - $version"
+
+git push -u release/$version
+
+git checkout master
+
+git merge --no-ff release/$version
+
+git push
+
+git tag -a $version -m "new release version $version"
+
+git push origin --tags
