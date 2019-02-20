914a5bb5f2e4974e9b6bcf088af17c7b63be909f
HarshaRangika <harsha@blimpit.com>
2019-02-20 09:26:53 +0530

    remove should

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-const should = require('should');
+// const should = require('should');
 const request = require('request');
 const expect = require('chai').expect;
 const baseUrl = "http://localhost:3004/api/";
